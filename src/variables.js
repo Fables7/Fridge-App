eb430190fe1df8d419d7815d645be0b59379d7aa
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2023-06-11 16:59:11 +0200

    Added variables file

---
+++ src/variables.js
@@ -0,0 +1,13 @@
+export const colors = {
+  // primary: '#23A7BC',
+  gray: '#656565',
+  lightGray: '#c6c7d9',
+  // turquoise: '#17C4C4',
+  turquoise: '#6FFFE9',
+  lightGreen: '#BED8AC',
+  primary: '#0B132B',
+  secondary: '#1C2541',
+  tertiary: '#3A506B',
+};
+
+export const API_URL = '';
