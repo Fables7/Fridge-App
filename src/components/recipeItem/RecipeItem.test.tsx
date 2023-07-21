ce72b4d2d8f22a00007ac08707007d6d105a40cc
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2023-07-21 17:18:51 +0100

    Fixed test for RecipeItem component by writing mock test fpr FontAwesomeIcon component

--- src/components/recipeItem/RecipeItem.test.tsx
+++ src/components/recipeItem/RecipeItem.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 import RecipeItem from './RecipeItem';
 
+jest.mock('@fortawesome/react-native-fontawesome', () => ({
+  FontAwesomeIcon: '',
+}));
 describe('RecipeItem', () => {
   it('should render correctly', () => {
     const {getByTestId} = render(
